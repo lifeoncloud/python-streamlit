8aabb91ab90dbe3e1c9f85be470a2b427e7eefb9
lifeoncloud <soozo0830@gmail.com>
2025-01-28 18:56:57 +0900

    add section2/rss_widget directory

---
+++ section2/rss_widget/rss.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import feedparser
+
+st.title("RSS 항목 정보 가져오기")
+rss_url = st.text_input("RSS 주소를 입력하세요.")
+
+if st.button("가져오기"):
+    if not rss_url:
+        st.warning("RSS 주소를 입력하세요.")
+    else:
+        feed = feedparser.parse(rss_url)
+        if "entries" in feed:
+            for entry in feed.entries:
+                st.write(f"제목:{entry.title}")
+                st.write(f"링크:{entry.link}")
+                st.write(f"요약:{entry.summary}")
+        else:
+            st.warning("해당 RSS 주소로부터 항목을 가져올 수 없습니다.")
+
+# pylint: disable=import-error
+# https://news.jtbc.co.kr/rss
+# https://news.sbs.co.kr/news/rss.do
+# https://cloud.google.com/feeds/gke-regular-channel-release-notes.xml
+# https://pylint.readthedocs.io/en/latest/user_guide/messages/error/import-error.html
